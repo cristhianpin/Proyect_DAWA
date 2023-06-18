0893efcc97da92cb57ec4f2bc8e8b6ff46d6ea1b
cristhianpin <cristhianpin92@gmail.com>
2023-06-18 16:52:02 -0500

    Tercer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { MenuRegistroComponent } from './menu-registro/menu-registro.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LoginComponent } from './login/login.component';
     HeaderComponent,
     FooterComponent,
     LoginComponent,
+    MenuRegistroComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu-registro/menu-registro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-registro',
+  templateUrl: './menu-registro.component.html',
+  styleUrls: ['./menu-registro.component.css']
+})
+export class MenuRegistroComponent {
+
+}
