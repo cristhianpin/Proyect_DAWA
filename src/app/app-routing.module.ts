b01eebd18ec6c8f7c58351746a5fc8f14a66b085
cristhianpin <cristhianpin92@gmail.com>
2023-06-18 17:53:18 -0500

    Cuarto commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent} from '../app/login/login.component';
+import { MenuRegistroComponent} from '../app/menu-registro/menu-registro.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: MenuRegistroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
